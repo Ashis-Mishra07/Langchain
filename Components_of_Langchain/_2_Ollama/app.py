ac0ca1339735b8a92b1d38f58e81aa99a8daafdd
Ashis-Mishra07 <mishralucky074@gmail.com>
2025-03-20 10:38:28 +0530

    web building with ollama done

---
+++ Components_of_Langchain/_2_Ollama/app.py
@@ -0,0 +1,46 @@
+# export all the env variables 
+
+import os 
+from dotenv import load_dotenv
+from langchain_community.llms import Ollama 
+
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+
+
+load_dotenv()
+# For Langsmith Tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
+
+
+# Prompt Template 
+
+prompt = ChatPromptTemplate.from_template(
+    "You are a helpful assistant. Please respond to the question asked.\n\nUser: {question}"
+)
+
+
+
+# Streamlit Framework
+
+st.title("Langchain Demo with Lamma2")
+input_text = st.text_input("Enter your question here" ) 
+
+
+# Call the Ollma model 
+llm = Ollama(model = "gemma:2b")             # google open source model 
+output_parser = StrOutputParser()    # to show the output on desired value
+
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
+
+
